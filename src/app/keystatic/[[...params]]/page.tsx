bcdb4e474abdd263098b67251fe3e79befa58a7a
Hari Krishna Anem <anemharikrishna@gmail.com>
2026-02-14 09:46:56 +0530

    fix: Remove prop spreading from makePage component - makePage returns a component that accepts no props

--- src/app/keystatic/[[...params]]/page.tsx
+++ src/app/keystatic/[[...params]]/page.tsx
@@ -3,14 +3,6 @@
 import { makePage } from "@keystatic/next/ui/app";
 import config from "../../../../keystatic.config";
 
-type Props = {
-  params: {
-    params?: string[];
-  };
-};
-
 const KeystaticApp = makePage(config);
 
-export default function Page(props: Props) {
-  return <KeystaticApp {...props} />;
-}
+export default KeystaticApp;
